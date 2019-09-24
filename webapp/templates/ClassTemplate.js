8705b2bbad1cfd304ffeb130da3d3a76d041e841
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-09-24 10:41:30 +0300

    Update ClassTemplate.js

--- webapp/templates/ClassTemplate.js
+++ webapp/templates/ClassTemplate.js
@@ -1,44 +1,15 @@
-// declare and document the constructor function
-sap.ui.base.Object.extend(
-  "ui5test.ui5test.templates.ClassTemplate",
-  /** @lends ui5test.templates.ClassTemplate */ {
-    constructor: function(sId, mProperties) {
-      // init and document members here
-      /**
-       * The ID of a ClassTemplate.
-       *
-       * @private
-       */
-      this.mId = sId;
-    },
-
-    // now add further methods to that prototype
-    /**
-     * Again a summary in one sentence.
-     *
-     * More details can be documented, when the method is more complex.
-     * @param {string} sMethod The same mechanism as above can be used to
-     *                         document the parameters.
-     * @param {object} [oListener] An optional parameter. If empty, the
-     *                             <code>window</code> is used instead.
-     * @experimental Since 1.24 Behavior might change.
-     * @public
-     */
-    ownMethod: function(sMethod, oListener) {
-      console.warn("called ownMethod!");
-    },
-
-    /**
-     * A private method.
-     *
-     * Every member with a doc comment is included in the public JSDOC.
-     * So we explicitly declare this as a private member:
-     *
-     * Additionally, using an underscore prefix prevents this method
-     * from beeing added to the public facade.
-     *
-     * @private
-     */
-    _myVeryPrivateMethod: function() {}
+sap.ui.define([
+  "sap/ui/base/Object",
+  "sap/ui/model/json/JSONModel"
+], function(Object,JSONModel) {
+  "use strict";
+  return Object.extend("be.wl.objects.model.BaseObject", {
+      constructor: function() {
+           this.model = new JSONModel();
+            this.model.setData(this);
+      },
+  getModel:function(){
+       return this.model;
   }
-);
+    });
+});
